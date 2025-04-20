bf670bd2dec734f35296bcca9c27bbb85574a7e1
chabarelado <josmachado48@gmail.com>
2025-04-20 12:32:52 +0000

    Super Trunfo mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,6 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
 int main() {
